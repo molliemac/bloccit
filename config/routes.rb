e5bba9bab00eec0813b937e7da402f650d6d2fe7
Mollie McIntyre <mollie.mcintyre9@gmail.com>
2017-04-13 02:10:52 -0700

    Assignment 19 completed - added Advertisement model, controller and view

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  	resources :posts
+ resources :advertisements
+ resources :posts
 
-   	get 'about' => 'welcome#about'
-
-   	root 'welcome#index'
-
+ get 'about' => 'welcome#about'
+ root 'welcome#index'
 end
