090353958ec056324cb190875818c5a9c2c4f234
Mollie McIntyre <mollie.mcintyre9@gmail.com>
2017-04-13 22:03:29 -0700

    Assignment 20 completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,7 +16,17 @@ require 'random_data'
      body: RandomData.random_paragraph
    )
  end
+
+ # Create Questions
+ 100.times do
+ 	Question.create!(
+ 		title: RandomData.random_sentence,
+ 		body: RandomData.random_paragraph,
+ 		resolved: false
+ 	)
+ end
  
  puts "Seed finished"
  puts "#{Post.count} posts created"
  puts "#{Comment.count} comments created"
+ puts "#{Question.count} questions created"
