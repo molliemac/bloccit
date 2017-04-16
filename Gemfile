9f00cd524d5b2f8b7357b57105bb340cbca11f32
Mollie McIntyre <mollie.mcintyre9@gmail.com>
2017-04-16 15:33:18 -0700

    Checkpoint 21 completed

--- Gemfile
+++ Gemfile
@@ -14,6 +14,7 @@
 
  group :development, :test do
  	gem 'rspec-rails', '~> 3.0'
+ 	gem 'shoulda'
  end
  
  # Use SCSS for stylesheets
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170413073600) do
+ActiveRecord::Schema.define(version: 20170416012904) do
 
   create_table "advertisements", force: :cascade do |t|
     t.string   "title"
@@ -35,6 +35,25 @@ ActiveRecord::Schema.define(version: 20170413073600) do
     t.text     "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "topic_id"
+  end
+
+  add_index "posts", ["topic_id"], name: "index_posts_on_topic_id"
+
+  create_table "questions", force: :cascade do |t|
+    t.string   "title"
+    t.text     "body"
+    t.boolean  "resolved"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "topics", force: :cascade do |t|
+    t.string   "name"
+    t.boolean  "public",      default: true
+    t.text     "description"
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
   end
 
 end
